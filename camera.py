eb4e08ddbc7a938bc5a8e8eef0d0bfd10063e223
Suzie Rhodes <magicqwertyuiop@gmail.com>
2019-02-15 12:48:17 -0700

    initial testing on how fast I can take pictures

--- camera.py
+++ camera.py
@@ -1,8 +1,19 @@
 from picamera import PiCamera
 from time import sleep
 
+# create the class to interact with the camera
 camera = PiCamera()
 
+# so the picture isn't uspide down the way I have it set up
+camera.rotation = 180
+
+# see what the camera sees for 3 secs
 camera.start_preview()
-sleep(10)
+sleep(5)
+camera.stop_preview()
+
+# take 5 pictures in a row
+camera.start_preview()
+for i in range(5):
+	camera.capture('/home/pi/BirdStrike/pictures/image%s.jpg' % i)
 camera.stop_preview()
