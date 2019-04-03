95a3a97dfb8ce702b424b0d878040a009eb4b3f2
Suzie Rhodes <magicqwertyuiop@gmail.com>
2019-04-03 13:27:30 -0600

    videos are now named with the time of the strike, added a .gitignore file

--- sensor.py
+++ sensor.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import picamera
 import io # I think I need this for the camera loop?
-
+import datetime
 
 # sensor set up
 channel = 27
@@ -12,11 +12,9 @@ GPIO.setup(channel, GPIO.IN)
 # camera set up
 camera = picamera.PiCamera()
 camera.rotation = 180
-'''
-camera.framerate = 90
-camera.resolution = '1280x720'
-camera.shutter_speed = 800
-'''
+camera.framerate = 75
+camera.resolution = '640x480'
+
 
 # set up a stream?
 stream = picamera.PiCameraCircularIO(camera, seconds = 6)
@@ -24,7 +22,7 @@ camera.start_recording(stream, format='h264')
 
 
 '''
-# what to do when sensor gets input
+# takes pictures when sensor reads input, too slow
 def callback(channel):
         if GPIO.input(channel):
             for i, filename in enumerate(camera.capture_continuous('%s/pictures/img{counter:03d}.jpg' % home)):
@@ -39,22 +37,16 @@ GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel, callback)  
 '''
 
-count = 0
 
 # keep the code running
 try:
     while True:
         camera.wait_recording(1)
         if GPIO.input(channel):
-            print("motion detected")
             camera.wait_recording(3)
-            stream.copy_to('motion%s.h264' % count)
-            count += 1
+            stream.copy_to('%s.h264' % datetime.datetime.now())
         else:
-            print("motion detected")
             camera.wait_recording(3)
-            stream.copy_to('motion%s.h264' % count)
-            count += 1
-            
+            stream.copy_to('%s.h264' % datetime.datetime.now())
 finally:
     camera.stop_recording()
